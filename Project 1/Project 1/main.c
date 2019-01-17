f86667afd53caca2d06349d91ced53f7d7ba2ce9
Jackson Tsoi <tsoijackson2@gmail.com>
2019-01-16 17:49:51 -0800

    Init Project 1

---
+++ Project 1/Project 1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project 1.c
+ *
+ * Created: 1/16/2019 5:49:38 PM
+ * Author : Jackson Tsoi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
