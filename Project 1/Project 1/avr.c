981a37ccf42ea0cd3d39a87707fae7e359a1d95b
Jackson Tsoi <tsoijackson2@gmail.com>
2019-01-16 18:00:38 -0800

    Added Given C Files

---
+++ Project 1/Project 1/avr.c
@@ -0,0 +1,20 @@
+/*
+ * avr.c
+ *
+ * Created: 1/16/2019 5:55:45 PM
+ *  Author: Jackson Tsoi
+ */ 
+
+#include "avr.h"
+
+void
+wait_avr(unsigned short msec)
+{
+	TCCR0 = 3;
+	while (msec--) {
+		TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.001);
+		SET_BIT(TIFR, TOV0);
+		while (!GET_BIT(TIFR, TOV0));
+	}
+	TCCR0 = 0;
+}
