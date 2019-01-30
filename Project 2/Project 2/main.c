95392edf8d44ade1b90733bfb7a17b136215c78d
Jackson Tsoi <tsoijackson2@gmail.com>
2019-01-30 12:47:53 -0800

    Import Project 2

---
+++ Project 2/Project 2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project 2.c
+ *
+ * Created: 1/30/2019 12:26:36 PM
+ * Author : Jackson Tsoi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
