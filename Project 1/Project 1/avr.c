1f326fb98e5dde1ebf2dc0156a1b1522e9b7172c
Jackson Tsoi <tsoijackson2@gmail.com>
2019-02-04 12:58:22 -0800

    Revert "Merge"
    This reverts commit 3a51866a39a996ee9c926e15870d4393782c56a0, reversing
    changes made to eedc30e66a40bebb3aab99b915a246aaa83a815c.

---
+++ Project 1/Project 1/avr.c
@@ -0,0 +1,28 @@
+/*
+ * avr.c
+ *
+ * Created: 1/16/2019 5:55:45 PM
+ *  Author: Jackson Tsoi
+ */ 
+
+#include "avr.h"
+
+void
+wait_avr(unsigned short msec)
+{
+	TCCR0 = 3;
+	while (msec--) {
+		TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.001);
+		SET_BIT(TIFR, TOV0);
+		while (!GET_BIT(TIFR, TOV0));
+	}
+	TCCR0 = 0;
+}
+
+void wait_half_sec() {
+	volatile int i;
+	unsigned long n = 50000;
+	for(i=0; i < n; ++i) {
+		/* nothing */
+	}
+}
---
+++ Project 1/Project 1/avr.h
@@ -0,0 +1,23 @@
+/*
+ * avr.h
+ *
+ * Created: 1/16/2019 5:54:56 PM
+ *  Author: Jackson Tsoi
+ */ 
+
+#ifndef _AVR_H
+#define _AVR_H
+
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <avr/io.h>
+
+#define XTAL_FRQ 8000000lu
+
+#define SET_BIT(p,i) ((p) |=  (1 << (i)))
+#define CLR_BIT(p,i) ((p) &= ~(1 << (i)))
+#define GET_BIT(p,i) ((p) &   (1 << (i)))
+
+void wait_avr(unsigned short msec);
+
+#endif /* _AVR_H */
