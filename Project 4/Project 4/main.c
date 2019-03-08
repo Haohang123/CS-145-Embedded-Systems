aa491f39aa93f05380ccef0d66f45e162460f54a
Jackson Tsoi <tsoijackson2@gmail.com>
2019-03-08 14:08:09 -0800

    Project 4 Update

--- Project 4/Project 4/main.c
+++ Project 4/Project 4/main.c
@@ -20,6 +20,10 @@ int TOTAL_SAMPLES = 0;
 float MAX_THRESHOLD = 4.5;
 float MIN_THRESHOLD = 0.5;
 
+float MAX_VOLTAGE = 5.0;
+int MAX_BITS = 1024;
+
+
 #define TOTAL_NOTES 12
 int FREQUENCY[TOTAL_NOTES] = {220, 233, 246, 261, 277, 293, 311, 329, 349, 369, 391, 415};
 int DURATION_MODIFIER[3] = {1,2,4};
@@ -81,11 +85,11 @@ void play_note(int frequency,int duration) {
 void AD_init() {
 	ADMUX=(1<<REFS0);
 	ADCSRA=(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+	ADMUX |= 0;
 }
 
 unsigned int get_AD() {
 	unsigned x;
-	ADMUX |= 0;
 	ADCSRA |= (1<<ADSC) | (1<<ADEN);
 	while(! (ADCSRA & (1<<ADIF) ));
 	ADCSRA |= (1<<ADIF) | (0<<ADEN);
@@ -110,10 +114,19 @@ void print_lcd() {
 			sprintf(buf2, "HI:---- LO:----");
 	}
 	else {
-		float high_converted = (float)(HIGH * 5.0/1024);
-		float low_converted = (float)(LOW * 5.0/1024);
-		float avg_converted = (float)(TOTAL_IV / TOTAL_SAMPLES * 5.0/1024);
-		float iv_converted = (float)(CURRENT_IV * 5.0/1024);
+		int modifier = 1;
+		float total_sum = 0;
+		
+		if (STATE == 2) {
+			modifier = -2;
+			total_sum = MAX_VOLTAGE;
+		} 
+		
+		float ratio = MAX_VOLTAGE / (MAX_BITS / modifier) ;
+		float high_converted = (float)(HIGH * ratio) + total_sum;
+		float low_converted = (float)(LOW * ratio) + total_sum;
+		float avg_converted = (float)(TOTAL_IV / TOTAL_SAMPLES * ratio) + total_sum;
+		float iv_converted = (float)(CURRENT_IV * ratio) + total_sum;
 		
 		sprintf(buf1, "IV:%d.%02d AV:%d.%02d", (int)(iv_converted),  (int)((iv_converted - (int)(iv_converted)) * 100), 
 										   (int)(avg_converted), (int)((avg_converted - (int)(avg_converted)) * 100));
@@ -136,6 +149,7 @@ int main(void)
 	avr_init();
 	lcd_init();
 	AD_init();
+	
     while (1) 
     {
 		if (STATE) {
@@ -161,16 +175,22 @@ int main(void)
 			TOTAL_SAMPLES ++;
 		}
 		
+		
 		int key = get_key() - 1;
 		if (key == -1) {
 			// Do Nothing
 		}
 		else if (keypad[key] == '1') { // START
+			ADMUX |= 0x0;
 			STATE = 1;
 		}
 		else if (keypad[key] == '2') { // RESET
+			ADMUX |= 0x0;
 			reset();
-		
+		}
+		else if (keypad[key] == '3') { // 2 STATE INPUT
+			ADMUX |= 0x10;
+			STATE = 2;
 		}
 		
 		print_lcd();
